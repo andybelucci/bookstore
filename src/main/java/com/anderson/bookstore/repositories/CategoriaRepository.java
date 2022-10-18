0b290629566e18cc302c2229ba45f31cee78bed2
andybelucci <beluccianderson@gmail.com>
2022-10-18 15:08:38 -0300

    Commit Instancia automática da base de dados

--- src/main/java/com/anderson/bookstore/repositories/CategoriaRepository.java
+++ src/main/java/com/anderson/bookstore/repositories/CategoriaRepository.java
@@ -3,7 +3,9 @@ package com.anderson.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.anderson.bookstore.domain.Categoria;
+
 @Repository
-public interface Categoria extends JpaRepository<com.anderson.bookstore.domain.Categoria, Integer>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
     
 }
--- src/main/java/com/anderson/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/anderson/bookstore/repositories/LivroRepository.java
@@ -3,7 +3,9 @@ package com.anderson.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.anderson.bookstore.domain.Livro;
+
 @Repository
-public interface Livro extends JpaRepository<com.anderson.bookstore.domain.Livro, Integer>{
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
     
 }
