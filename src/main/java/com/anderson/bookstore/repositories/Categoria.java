66706bac8543053177afef32065e502dee4a3b01
andybelucci <beluccianderson@gmail.com>
2022-10-18 13:31:18 -0300

    Commit Repositories implementados

---
+++ src/main/java/com/anderson/bookstore/repositories/Categoria.java
@@ -0,0 +1,9 @@
+package com.anderson.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Categoria extends JpaRepository<com.anderson.bookstore.domain.Categoria, Integer>{
+    
+}
