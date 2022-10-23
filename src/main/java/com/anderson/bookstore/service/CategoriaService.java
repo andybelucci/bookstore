8c584d883d1030637eb57d5217479b9948475458
andybelucci <beluccianderson@gmail.com>
2022-10-22 21:04:53 -0300

    Commit Exceção personalizada para objeto não encontrado

--- src/main/java/com/anderson/bookstore/service/CategoriaService.java
+++ src/main/java/com/anderson/bookstore/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.anderson.bookstore.service;
 
 import com.anderson.bookstore.domain.Categoria;
 import com.anderson.bookstore.repositories.CategoriaRepository;
+import com.anderson.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class CategoriaService {
 
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
     }
 }
---
+++ src/main/java/com/anderson/bookstore/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,17 @@
+package com.anderson.bookstore.service.exceptions;
+
+import java.io.Serial;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
