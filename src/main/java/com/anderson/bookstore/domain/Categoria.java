e53f14dc6797197da2f58d8bb98ff6956d2ff3d8
andybelucci <beluccianderson@gmail.com>
2022-10-24 09:31:52 -0300

    Commit FindAll para Categoria utilizando padrão DTO

--- src/main/java/com/anderson/bookstore/domain/Categoria.java
+++ src/main/java/com/anderson/bookstore/domain/Categoria.java
@@ -1,5 +1,6 @@
 package com.anderson.bookstore.domain;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Categoria implements Serializable{
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
